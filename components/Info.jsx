4c294dbdf65d92286d359cac0b43edbed135da43
Silas Lenihan <sjl@lenihan.net>
2022-07-26 17:56:54 -0400

    initial commit

---
+++ components/Info.jsx
@@ -0,0 +1,9 @@
+
+
+export default function Info () {
+    return (
+
+        <p className="pt-6 text-left text-white">Welcome to my perosnal website. Here you will find information about myself,
+        the projects I have been working on, as well as my hobbies. </p>
+    )
+}
